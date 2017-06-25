554617d85be1cc19c705e23f6279b8426dd180ce
Jeremy Hull <jchull@coderado-mbp.local>
2017-06-25 16:16:18 -0600

    Warning message and link when API key is not set, added simple instructions to readme

--- src/App.js
+++ src/App.js
@@ -13,10 +13,14 @@ class App extends Component {
                  alt="logo"/>
             <h2>React Weather</h2>
           </div>
-          {process.env.REACT_APP_WEATHER_API_KEY.endsWith("WeatherUnderground!") &&
-            <div className="warn">{process.env.REACT_APP_WEATHER_API_KEY}</div>
+          {process.env.REACT_APP_WEATHER_API_KEY.endsWith("WeatherUnderground!") ?
+              <div className="warn">
+                <span>{process.env.REACT_APP_WEATHER_API_KEY}</span><br/>
+                <a href="https://www.wunderground.com/weather/api">Weather Underground API</a>
+              </div>
+              :
+              <StationSearch></StationSearch>
           }
-          <StationSearch></StationSearch>
         </div>
     );
   }
